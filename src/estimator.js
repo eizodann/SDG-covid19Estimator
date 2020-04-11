81ef87bcdb490bd75c8cf59926b2060a25f372c5
Enoch Ezeani <40115382+eizodann@users.noreply.github.com>
2020-04-11 20:28:00 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -5,9 +5,8 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = Math.floor(data.reportedCases * 10);
   severeImpact.currentlyInfected = Math.floor(data.reportedCases * 50);
 
-//   est no of infected by days
-  impact.infectionsByRequestedTime = Math.floor(impact.currentlyInfected * (2**10));
-  severeImpact.infectionsByRequestedTime = Math.floor(severeImpact.currentlyInfected * (2**10));
+  impact.infectionsByRequestedTime = Math.floor(impact.currentlyInfected * (2 ** 10));
+  severeImpact.infectionsByRequestedTime = Math.floor(severeImpact.currentlyInfected * (2 ** 10));
 
   return {
     data,
