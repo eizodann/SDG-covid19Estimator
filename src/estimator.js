42f530b77eed6583b46d790e175ec4020409bd7b
Enoch Ezeani <40115382+eizodann@users.noreply.github.com>
2020-04-11 19:53:32 +0100

    first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,14 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  let impact = {};
+  let severeImpact = {};
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  //est no of infected by days
+  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2,10)
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2,10)
+};
+
 
 export default covid19ImpactEstimator;
